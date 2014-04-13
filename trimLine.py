97075b7171b70f25bd81328fd89467b4a4a09ed4
guanghua.zhu <guanghua.zhu@dianping.com>
2014-04-13 23:43:52 +0800

    trimLine

---
+++ trimLine.py
@@ -0,0 +1,27 @@
+import os
+import string
+import shutil
+
+def main(root):
+    names = os.listdir(root)
+    with open(root + '.txt', 'w') as mergedTxt:
+        for name in names:
+            process(root, name, mergedTxt)
+
+def process(root, name, mergedTxt):
+    fname, fext = os.path.splitext(name)
+    if(fext != '.txt'):
+        print name + ' is not text'
+        return
+    path = os.path.join(root, name)
+    #mergedTxt.write('\n\n')
+    #mergedTxt.write('-------')
+    #mergedTxt.write(name)
+    #mergedTxt.write('-------')
+    #mergedTxt.write('\n')
+    with open(path) as text:
+        lines = text.read().split('\n')
+        for line in lines[2:len(lines) - 7]:
+            mergedTxt.write(line + '\n')
+    print name
+
