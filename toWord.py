14fb1a953956fcb88edfc97d0608838dec57765b
朱光华 <guanghua@zhughld.local>
2017-08-20 23:32:24 +0800

    mergeWord

--- toWord.py
+++ toWord.py
@@ -3,30 +3,39 @@ import string
 import shutil
 
 mergeLetters = {'b':'q', 'd':'p', 'n':'u'}
-# mergeWords = dict()
-topLimit = 3000
+mergeWords = dict()
+topLimit = 5000
 lengthLimit = 3 + 1
 rootPath = '../../Downloads/sentence/'
 
 
-def main():
+def merge_words():
     # names = os.listdir(root)
     index = 0
     with open(rootPath + '10000.txt') as wordFile:
-        with open(rootPath + 'topLimit.txt', 'w') as limitFile:
             for item in wordFile:
                 index += 1
                 if index > topLimit:
                     break
                 if item.__len__() < lengthLimit:
                     continue
-                limitFile.write(item)
-                print(str(index) + ":" + item.rstrip())
-                print(item.__len__())
-                # key = fields[1]
-                # mergeValue = mergeWords[key]
-                # if(mergeWords[key] == None) :
-                #     mergeWords.update(key, )
+                item = item.rstrip()
+                key = merge_letter(item)
+                values = mergeWords.get(key)
+                if values is None:
+                    values = []
+                    mergeWords[key] = values
+                values.append(item)
+    print(mergeWords)
+    with open(rootPath + 'topLimit.txt', 'w') as limitFile:
+        for key, values in mergeWords.items():
+            limitFile.write(key + ':' + str(values) + '\n')
 
 
-main()
+def merge_letter(word):
+    for key, value in mergeLetters.items():
+        word = word.replace(value, key)
+    return word
+
+
+merge_words()
