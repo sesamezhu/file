7dc998c3d7ea9a30fe421c9cb3ef8a46b712779f
朱光华 <guanghua@zhughld.local>
2017-08-21 23:19:03 +0800

    key_chars

--- toWord.py
+++ toWord.py
@@ -2,6 +2,7 @@ import os
 import string
 import shutil
 
+theLetters = string.ascii_lowercase
 mergeLetters = {'b':'q', 'd':'p', 'n':'u'}
 mergeWords = dict()
 topLimit = 5000
@@ -10,7 +11,6 @@ rootPath = '../../Downloads/sentence/'
 
 
 def merge_words():
-    # names = os.listdir(root)
     index = 0
     with open(rootPath + '10000.txt') as wordFile:
             for item in wordFile:
@@ -26,7 +26,7 @@ def merge_words():
                     values = []
                     mergeWords[key] = values
                 values.append(item)
-    print(mergeWords)
+    # print(mergeWords)
     with open(rootPath + 'topLimit.txt', 'w') as limitFile:
         for key, values in mergeWords.items():
             limitFile.write(key + ':' + str(values) + '\n')
@@ -38,4 +38,50 @@ def merge_letter(word):
     return word
 
 
-merge_words()
+def main():
+    merge_words()
+    letters = list(merge_letter(theLetters))
+    key_chars = []
+    for key in mergeWords.keys():
+        key_chars.append(list(key))
+    result_chars = []
+    for chars in key_chars:
+        if letters.__len__() < lengthLimit:
+            break
+        if contain_all(letters, chars):
+            result_chars.append(chars)
+            remove_all(letters, chars)
+    print(str(letters))
+    print(str_chars(result_chars))
+
+
+def str_chars(charss):
+    result = []
+    for chars in charss:
+        result.append(mergeWords[''.join(chars)])
+    return result
+
+
+def contain_all(letters, chars):
+    check_all = copy_list(letters)
+    for char in chars:
+        if not check_all.__contains__(char):
+            return False
+        else:
+            check_all.remove(char)
+    return True
+
+
+def copy_list(lst):
+    result = []
+    for item in lst:
+        result.append(item)
+    return result
+
+
+def remove_all(letters, chars):
+    for char in chars:
+        letters.remove(char)
+
+
+main()
