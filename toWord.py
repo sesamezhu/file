2a20f86677124aac7e17c1490585382b25fcfa13
朱光华 <guanghua@zhughld.local>
2017-08-22 00:40:16 +0800

    key_chars

--- toWord.py
+++ toWord.py
@@ -2,12 +2,19 @@ import os
 import string
 import shutil
 
+
+class Counter:
+    index = 0
+
+
 theLetters = string.ascii_lowercase
 mergeLetters = {'b':'q', 'd':'p', 'n':'u'}
 mergeWords = dict()
-topLimit = 5000
+sortedKeys = []
+topLimit = 3000
 lengthLimit = 3 + 1
 rootPath = '../../Downloads/sentence/'
+counter = Counter
 
 
 def merge_words():
@@ -25,8 +32,8 @@ def merge_words():
                 if values is None:
                     values = []
                     mergeWords[key] = values
+                    sortedKeys.append(list(key))
                 values.append(item)
-    # print(mergeWords)
     with open(rootPath + 'topLimit.txt', 'w') as limitFile:
         for key, values in mergeWords.items():
             limitFile.write(key + ':' + str(values) + '\n')
@@ -41,24 +48,36 @@ def merge_letter(word):
 def main():
     merge_words()
     letters = list(merge_letter(theLetters))
-    key_chars = []
-    for key in mergeWords.keys():
-        key_chars.append(list(key))
+    key_chars = sortedKeys
+    while key_chars.__len__() > 100:
+        first_index = from_index(copy_list(letters), key_chars)
+        key_chars = key_chars[first_index:]
+
+
+def from_index(letters, key_chars):
     result_chars = []
+    first_index = 0
+    not_matched = True
     for chars in key_chars:
+        if not_matched:
+            first_index += 1
         if letters.__len__() < lengthLimit:
             break
         if contain_all(letters, chars):
+            not_matched = False
             result_chars.append(chars)
             remove_all(letters, chars)
-    print(str(letters))
-    print(str_chars(result_chars))
+    if letters.__len__() < 7:
+        counter.index += 1
+        print(str(counter.index) + "-" + str(key_chars.__len__()) + " -- " + ','.join(letters))
+        print(','.join(str_chars(result_chars)))
+    return first_index
 
 
 def str_chars(charss):
     result = []
     for chars in charss:
-        result.append(mergeWords[''.join(chars)])
+        result.append('|'.join(mergeWords[''.join(chars)]))
     return result
 
 
